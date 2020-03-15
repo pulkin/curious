5635bc4ad548fbe48d603d45a7c3e0e63e4346ab
Artem <gpulkin@gmail.com>
2020-03-15 01:58:01 +0100

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author='Artem Pulkin',
     description='Feature-driven function sampling',
     ext_modules=[Extension('cutil', sources=['c/cutil.c'], include_dirs=[numpy.get_include()])],
-    scripts=['scripts/curious.py'],
+    scripts=['scripts/curious.py'], requires=['numpy', 'scipy', 'matplotlib', 'imageio']
 )
