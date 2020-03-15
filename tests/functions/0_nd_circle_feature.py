f06248a2c52e63c5d9bd7dab504d4937a1e4bd09
Artem <gpulkin@gmail.com>
2020-03-15 19:49:29 +0100

    Fix issue in the sample function

--- tests/functions/0_nd_circle_feature.py
+++ tests/functions/0_nd_circle_feature.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import sys
-x = map(float, sys.argv[1:])
-r = sum(float(i) ** 2 for i in x) ** .5
-p = 1
+x = tuple(map(float, sys.argv[1:]))
+r = sum(i ** 2 for i in x) ** .5
+p = 1.0
 for i in x:
     p *= i
 print(1. / abs(r-0.5-0.1j)**2 + p)
